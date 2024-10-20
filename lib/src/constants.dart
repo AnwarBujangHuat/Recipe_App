2df37d00dfa095f40c1e42c5279d64d8000fdfb4
Anwar Chong <anwarbinbujang@gmail.com>
2024-10-20 15:48:34 +0800

    pushing

---
+++ lib/src/constants.dart
@@ -0,0 +1,8 @@
+class RouteName {
+  static String loginPage = 'loginPage';
+  static String homePage = 'homePage';
+  static String detailsPage = 'detailsPage';
+  static String addNewRecipePage = 'addNewRecipePage';
+}
+
+class AppColors {}
